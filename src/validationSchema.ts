1b6c8e206c9ce682106cd33b0cab2b5872ce203c
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2024-07-24 18:04:09 +1000

    chore: Add react-zoom-pan-pinch library for image zooming and panning

--- src/validationSchema.ts
+++ src/validationSchema.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 
 const validationSchema = Yup.object().shape({
   driver_name: Yup.string().required('Driver name is required'),
-  date: Yup.string().required('Date is required'),
+  date: Yup.date().required('Date is required'),
   clock_on_time: Yup.string().required('Clock-on time is required'),
   clock_off_time: Yup.string().required('Clock-off time is required'),
   odo_start: Yup.string().required('ODO start is required'),
